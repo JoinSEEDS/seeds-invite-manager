3adbdb7eebb1d158a3f84b0af10cb6e78f8444c5
Apostol Apostolov <apostolov87@gmail.com>
2021-09-04 18:14:03 +0300

    remove /dist/ folder - because it's compiled

--- dist/htmlHelpers/toJSON.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toJSON = void 0;
-function toJSON(obj) {
-    return JSON.stringify(obj, null, 3);
-}
-exports.toJSON = toJSON;
-;
-//# sourceMappingURL=toJSON.js.map
--- dist/infrastructure/routeManager.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prefix = void 0;
-var urlPrefix = '/network';
-function prefix(initialUrl) {
-    return urlPrefix + initialUrl;
-}
-exports.prefix = prefix;
-//# sourceMappingURL=routeManager.js.map
